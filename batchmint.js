61aac8076220ff77f3a8ffaf528e667dd1e21b17
Akshit9604 <132479119+Akshit9604@users.noreply.github.com>
2024-09-30 23:25:44 +0530

    Create batchmint.js

---
+++ batchmint.js
@@ -0,0 +1,40 @@
+// This script batch mints lord ERC721A tokens.
+
+// Import required libraries
+const { ethers } = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  // Get private key from env
+  const privateKey = process.env.PRIVATE_KEY;
+
+  // The URL of the network provider
+  const networkAddress =
+    "https://ethereum-sepolia-rpc.publicnode.com/";
+
+  // Create a provider using the URL
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
+
+  // Create a signer from the private key and provider
+  const signer = new ethers.Wallet(privateKey, provider);
+
+  // Tthe address of the deployed contract
+  const contractAddress = "0xa8d947C4C9203C7d9Eb4efb91539E4fc0379B37D";
+
+  // Get the contract factory and attach it to the signer
+  const IndianNFT = await ethers.getContractFactory("monastery", signer);
+  const contract = await IndianNFT.attach(contractAddress);
+
+  // Call the mint function on the contract to mint 5 tokens
+  await contract.mint(5);
+
+  // Log a message to the console to indicate that the tokens have been minted
+  console.log("Minted 5 tokens");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
